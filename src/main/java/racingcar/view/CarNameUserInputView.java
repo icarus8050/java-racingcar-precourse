3399ed565850905fefab55c8c9346ad4df9fc568
ChulYun <chul@finda.co.kr>
2021-05-02 14:44:27 +0900

    reactor: 자동차 이름 입력 뷰를 쉼표 기준으로 구분 시켜주는 역할로 축소

--- src/main/java/racingcar/view/CarNameUserInputView.java
+++ src/main/java/racingcar/view/CarNameUserInputView.java
@@ -1,27 +1,18 @@
 package racingcar.view;
 
-import racingcar.car.RacingCars;
 import racingcar.util.StringSeparator;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CarNameUserInputView {
 
-    public Optional<RacingCars> inputCarNames(Scanner scanner) {
+    public List<String> inputCarNames(Scanner scanner) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] separatedCarNames = StringSeparator.separate(scanner.nextLine());
-        List<String> carNames = toCollection(separatedCarNames);
-
-        try {
-            return Optional.of(RacingCars.of(carNames));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return Optional.empty();
-        }
+        return toCollection(separatedCarNames);
     }
 
     public List<String> toCollection(String[] separatedCarNames) {
