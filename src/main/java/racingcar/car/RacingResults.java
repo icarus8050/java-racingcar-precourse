322055e156eeb1378c227d4f687dcbaacbfb0757
ChulYun <chul@finda.co.kr>
2021-05-02 13:51:57 +0900

    feat: 경기 결과 중, 우승한 차량을 선출하는 기능 구현

--- src/main/java/racingcar/car/RacingResults.java
+++ src/main/java/racingcar/car/RacingResults.java
@@ -1,5 +1,6 @@
 package racingcar.car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingResults {
@@ -23,5 +24,46 @@ public class RacingResults {
 
     public RacingCarInfo nextResult() {
         return racingCarInfos.get(index++);
+    }
+
+    public WinningCars electWinningCars() {
+        int maxDistance = findMaxDistance();
+        return findWinningCarsByMaxDistance(maxDistance);
+    }
+
+    private int findMaxDistance() {
+        int max = 0;
+        for (RacingCarInfo racingCarInfo : racingCarInfos) {
+            max = Math.max(max, racingCarInfo.getDistance());
+        }
+        return max;
+    }
+
+    private WinningCars findWinningCarsByMaxDistance(int maxDistance) {
+        WinningCars winningCars = new WinningCars(maxDistance);
+        for (RacingCarInfo racingCarInfo : racingCarInfos) {
+            winningCars.addWinningCarInfoIfMaxDistance(racingCarInfo);
+        }
+        return winningCars;
+    }
+
+    public static class WinningCars {
+        private final int maxDistance;
+        private final List<RacingCarInfo> winningCars;
+
+        public WinningCars(int maxDistance) {
+            this.maxDistance = maxDistance;
+            this.winningCars = new ArrayList<>();
+        }
+
+        private void addWinningCarInfoIfMaxDistance(RacingCarInfo racingCarInfo) {
+            if (racingCarInfo.getDistance() == maxDistance) {
+                winningCars.add(racingCarInfo);
+            }
+        }
+
+        public List<RacingCarInfo> getWinningCars() {
+            return winningCars;
+        }
     }
 }
---
+++ src/test/java/racingcar/car/RacingResultsTest.java
@@ -0,0 +1,50 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingResultsTest {
+
+    @Test
+    @DisplayName("단독 우승한 차량 선출 테스트")
+    void electWinningCarTest() {
+        List<RacingCarInfo> participants = Arrays.asList(
+                new RacingCarInfo("a", 5),
+                new RacingCarInfo("b", 1),
+                new RacingCarInfo("c", 6),
+                new RacingCarInfo("d", 5)
+        );
+        RacingResults racingResults = new RacingResults(participants);
+        RacingResults.WinningCars winningCars = racingResults.electWinningCars();
+
+        List<RacingCarInfo> racingCarInfos = winningCars.getWinningCars();
+        for (RacingCarInfo racingCarInfo : racingCarInfos) {
+            assertThat(racingCarInfo.getCarName()).isEqualTo("c");
+            assertThat(racingCarInfo.getDistance()).isEqualTo(6);
+        }
+    }
+
+    @Test
+    @DisplayName("공동 우승한 차량 선출 테스트")
+    void electWinningCarsTest() {
+        List<RacingCarInfo> participants = Arrays.asList(
+                new RacingCarInfo("a", 6),
+                new RacingCarInfo("b", 1),
+                new RacingCarInfo("c", 6),
+                new RacingCarInfo("d", 5)
+        );
+        RacingResults racingResults = new RacingResults(participants);
+        RacingResults.WinningCars winningCars = racingResults.electWinningCars();
+
+        List<RacingCarInfo> racingCarInfos = winningCars.getWinningCars();
+        for (RacingCarInfo racingCarInfo : racingCarInfos) {
+            assertThat(racingCarInfo.getCarName()).isIn("a", "c");
+            assertThat(racingCarInfo.getDistance()).isEqualTo(6);
+        }
+    }
+}
