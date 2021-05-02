bf0952c25d25373d0ba370d96ebbb364224c3427
ChulYun <chul@finda.co.kr>
2021-05-02 16:26:01 +0900

    test: RacingCar 전진 및 멈춤 테스트 코드 추가

--- src/test/java/racingcar/car/RacingCarTest.java
+++ src/test/java/racingcar/car/RacingCarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RacingCarTest {
@@ -20,5 +21,25 @@ public class RacingCarTest {
     @DisplayName("생성되는 자동차의 이름 길이가 0 또는 6 이상이면 예외")
     void failedCreationCarTest(String invalidCarName) {
         assertThrows(IllegalArgumentException.class, () -> RacingCar.of(invalidCarName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    @DisplayName("입력 값이 4 미만인 경우 멈춤")
+    void stopCarTest(int driveValue) {
+        RacingCar racingCar = RacingCar.of("abc");
+        racingCar.drive(driveValue);
+
+        assertThat(racingCar.getDistance()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    @DisplayName("입력 값이 4 이상인 경우 전진")
+    void driveCarTest(int driveValue) {
+        RacingCar racingCar = RacingCar.of("abc");
+        racingCar.drive(driveValue);
+
+        assertThat(racingCar.getDistance()).isEqualTo(1);
     }
 }
